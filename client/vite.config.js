b6ea2048bfb52019995f98316083b304c5859158
Madhu. Siripurapu <120372287+Gabbumaay@users.noreply.github.com>
2025-04-23 12:08:42 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,20 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173
+    port: process.env.PORT || 5173,
+    allowedHosts: [
+      'mediease-client.onrender.com',
+      'localhost',
+      '127.0.0.1'
+    ]
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173
+    port: process.env.PORT || 5173,
+    allowedHosts: [
+      'mediease-client.onrender.com',
+      'localhost',
+      '127.0.0.1'
+    ]
   }
 })
